d70c8b59d1839497f021515188a24f7ed568b8fa
Ju Young <jjystar3@gmail.com>
2024-07-02 15:58:21 +0900

    method 함수 만들기

--- src/method/Ex01.java
+++ src/method/Ex01.java
@@ -4,8 +4,34 @@ public class Ex01 {
 
 	public static void main(String[] args) {
 		
+		method1();
 		
+		String str = method2();
+		System.out.println(str);
+		
+		method3(15,20);
+		
+		int i = method4(20,50);
+		System.out.println(i);
 		
 	}
-
+	
+	public static void method1() {
+		System.out.println("매개변수와 리턴타입이 둘 다 없는 메소드");
+		return; // 생략 가능
+	}
+	
+	public static String method2() {
+		return "매개변수가 없지만 반환값이 있는 메소드";
+	}
+	
+	public static void method3(int num1, int num2) {
+		int sum = num1 + num2;
+		System.out.println("sum : " + sum);
+	}
+	
+	public static int method4(int num1, int num2) {
+		return num1 + num2;
+	}
+	
 }
