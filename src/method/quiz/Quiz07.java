95018125d60bc59471b15d6159784a4e3692f953
Ju Young <jjystar3@gmail.com>
2024-07-03 22:52:55 +0900

    함수 기능 구현 예제 수정

--- src/method/quiz/Quiz07.java
+++ src/method/quiz/Quiz07.java
@@ -12,8 +12,8 @@ public class Quiz07 {
 	public static void function(int num) {
 
 		int n1 = num / 100;
-		int n2 = (num - n1 * 100) / 10;
-		int n3 = num - n1 * 100 - n2 * 10;
+		int n2 = num % 100 / 10;
+		int n3 = num % 10;
 
 		int result = n1 + n2 + n3;
 		System.out.println(n1 + " + " + n2 + " + " + n3 + " = " + result);
